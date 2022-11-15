e00fd47e8112bdc54f9039633bf9620e3d5390a8
sidneymaleo <sidney.maleo@socloz.com>
2022-11-15 03:04:17 +0100

    We upgrade our ui

--- app/src/main/java/com/android/yambasama/ui/views/bottomnavigationviews/searchView/SearchLocation.kt
+++ app/src/main/java/com/android/yambasama/ui/views/bottomnavigationviews/searchView/SearchLocation.kt
@@ -1,15 +1,33 @@
 package com.android.yambasama.ui.views.bottomnavigationviews.searchView
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.android.yambasama.R
 import kotlinx.coroutines.delay
+import java.util.*
 
 
 @ExperimentalMaterial3Api
@@ -18,44 +36,71 @@ fun SearchLocation(navController: NavHostController) {
     var visibleSearch by remember { mutableStateOf(false) }
     val listState = rememberLazyListState()
     val context = LocalContext.current
-    //val locationList = mutableStateListOf<Location>()
-    //val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-
-    /*locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))
-    locationList.add(Location("Aeroport Maya Maya (Brazzaville)"))*/
+    var searchLocation by rememberSaveable { mutableStateOf("") }
 
 
-    Scaffold(topBar = {
-        TopAppBar(
-            title = {},
-            navigationIcon = {
-                IconButton(onClick = {
-                }) {
-                    Icon(
-                        imageVector = Icons.Outlined.ArrowBack,
-                        contentDescription = null
+    AnimatedVisibility(
+        visible = visibleSearch,
+        enter = slideInHorizontally(animationSpec = tween(durationMillis = 200)) { fullWidth ->
+            // Offsets the content by 1/3 of its width to the left, and slide towards right
+            // Overwrites the default animation with tween for this slide animation.
+            -fullWidth / 3
+        } + fadeIn(
+            // Overwrites the default animation with tween
+            animationSpec = tween(durationMillis = 200)
+        ),
+        exit = slideOutHorizontally(animationSpec = spring(stiffness = Spring.StiffnessHigh)) {
+            // Overwrites the ending position of the slide-out to 200 (pixels) to the right
+            200
+        } + fadeOut()
+    ) {
+
+        Scaffold(topBar = {
+            Column (
+                modifier = Modifier.background(MaterialTheme.colorScheme.surface)) {
+                Row {
+                    OutlinedTextField(
+                        value = searchLocation,
+                        singleLine = true,
+                        textStyle = TextStyle( fontSize = 12.sp),
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                        ),
+                        onValueChange = {locationName ->
+
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeholder = { Text(text= stringResource(id = R.string.search), fontSize = 12.sp) },
+                        leadingIcon = {
+                            IconButton(onClick = { }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Search,
+                                    contentDescription = "",
+                                    tint = MaterialTheme.colorScheme.primary
+                                )
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 10.dp, bottom = 10.dp, start = 30.dp, end = 30.dp)
+                            .height(45.dp),
+                        shape = RoundedCornerShape(22.dp)
                     )
                 }
-            },
-            actions =  {},
-            scrollBehavior = scrollBehavior
-        )
-    },
-        content= {innerPadding ->
-            LazyColumn(contentPadding = innerPadding, state = listState) {
-                items(1000) {location ->
-                    SearchTownItem()
-                }
             }
-    })
+
+        },
+            content= {innerPadding ->
+                LazyColumn(contentPadding = innerPadding, state = listState) {
+                    items(1000) {location ->
+                        SearchTownItem()
+                    }
+                }
+            })
+    }
+
+
 
     LaunchedEffect(true) {
         delay(3)
--- app/src/main/java/com/android/yambasama/ui/views/bottomnavigationviews/searchView/SearchView.kt
+++ app/src/main/java/com/android/yambasama/ui/views/bottomnavigationviews/searchView/SearchView.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -25,6 +26,9 @@ import androidx.navigation.NavHostController
 import com.android.yambasama.R
 import com.android.yambasama.ui.views.model.Route
 import kotlinx.coroutines.delay
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import java.util.*
 
 @ExperimentalMaterial3Api
 @Composable
@@ -33,6 +37,35 @@ fun SearchView(navController: NavHostController) {
     var destination by rememberSaveable { mutableStateOf("") }
     var travelDate by rememberSaveable { mutableStateOf("") }
     var enabled by remember { mutableStateOf(false) }
+    // Fetching the Local Context
+    val mContext = LocalContext.current
+    // Declaring integer values
+    // for year, month and day
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
+    // Initializing a Calendar
+    val mCalendar = Calendar.getInstance()
+
+    // Fetching current year, month and day
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    mCalendar.time = Date()
+
+    // Declaring a string value to
+    // store date in string format
+    val mDate = remember { mutableStateOf("") }
+
+    // Declaring DatePickerDialog and setting
+    // initial values as current values (present year, month and day)
+    val mDatePickerDialog = DatePickerDialog(
+        mContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
 
     Column(
         modifier = Modifier
@@ -40,87 +73,106 @@ fun SearchView(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        OutlinedTextField(
-            value = departure,
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
-            ),
-            onValueChange = {
-                departure = it
-            },
-            label = { Text(stringResource(id = R.string.departure)) },
-            placeholder = { Text("") },
-            enabled = enabled,
-            leadingIcon = {
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Outlined.FlightTakeoff,
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-            },
-            modifier = Modifier
-                .clickable {
-                    navController.navigate(Route.searchLocalizeView)
-                    Log.d("MALEO9393", "MALEO Test")
-                }
-                .padding(top = 0.dp, bottom = 0.dp, start = 0.dp, end = 0.dp),
-            shape = RoundedCornerShape(12.dp)
-        )
-
-        OutlinedTextField(
-            value = destination,
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
-            ),
-            onValueChange = { destination = it },
-            label = { Text(stringResource(id = R.string.destination)) },
-            placeholder = { Text("") },
-            //enabled = false,
-            leadingIcon = {
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Outlined.FlightLand,
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-            },
-            modifier = Modifier
-                .padding(top = 20.dp, bottom = 0.dp, start = 0.dp, end = 0.dp),
-            shape = RoundedCornerShape(12.dp)
-        )
-
-        OutlinedTextField(
-            value = travelDate,
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
-            ),
-            onValueChange = { travelDate = it },
-            label = { Text(stringResource(id = R.string.travel_date)) },
-            placeholder = { Text("") },
-            //enabled = false,
-            leadingIcon = {
-                IconButton(onClick = {
-
-                }) {
-                    Icon(
-                        imageVector = Icons.Outlined.Today,
-                        contentDescription = "",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-            },
-            modifier = Modifier
-                .padding(top = 20.dp, bottom = 0.dp, start = 0.dp, end = 0.dp),
-            shape = RoundedCornerShape(12.dp)
-        )
 
         OutlinedButton(
             modifier = Modifier
                 .width(280.dp)
-                .padding(top = 30.dp),
+                .height(55.dp),
+            border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.primary),
+            shape = RoundedCornerShape(30),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)) ,
+            onClick = {
+                navController.navigate(Route.searchLocalizeView)
+            }) {
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row {
+                    Icon(
+                        imageVector = Icons.Outlined.FlightTakeoff,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(
+                        text = if(departure.length > 3) { departure } else {stringResource(R.string.departure)},
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedButton(
+            modifier = Modifier
+                .width(280.dp)
+                .height(55.dp),
+            border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.primary),
+            shape = RoundedCornerShape(30),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)) ,
+            onClick = {
+                navController.navigate(Route.searchLocalizeView)
+            }) {
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row {
+                    Icon(
+                        imageVector = Icons.Outlined.FlightLand,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(
+                        text = if(departure.length > 3) { departure } else {stringResource(R.string.destination)},
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedButton(
+            modifier = Modifier
+                .width(280.dp)
+                .height(55.dp),
+            border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.primary),
+            shape = RoundedCornerShape(30),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)) ,
+            onClick = {
+                mDatePickerDialog.show()
+            }) {
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row {
+                    Icon(
+                        imageVector = Icons.Outlined.Today,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(
+                        text = if(mDate.value.length > 3) { mDate.value } else {stringResource(R.string.travel_date)},
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedButton(
+            modifier = Modifier
+                .width(280.dp),
             border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.primary),
             onClick = {
                 //navController.navigate(Route.homeView)
