2de0dd80cdb36f9fd3fe0022b6eb0f6c5f99c933
sidneymaleo <sidney.maleo@socloz.com>
2022-12-13 02:50:41 +0100

    Display our profile picture

--- app/src/main/java/com/android/yambasama/ui/views/HomeAppView.kt
+++ app/src/main/java/com/android/yambasama/ui/views/HomeAppView.kt
@@ -1,21 +1,39 @@
 package com.android.yambasama.ui.views
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.*
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
+import com.android.yambasama.BuildConfig
 import com.android.yambasama.R
 import com.android.yambasama.presentation.viewModel.drop.DropViewModel
 import com.android.yambasama.presentation.viewModel.user.UserViewModel
@@ -24,6 +42,7 @@ import com.android.yambasama.ui.views.bottomnavigationviews.AddAdView
 import com.android.yambasama.ui.views.model.BottomNavigationItem
 import com.android.yambasama.ui.views.model.Route
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 
 @Composable
 @ExperimentalMaterial3Api
@@ -38,7 +57,6 @@ fun HomeApp(
     val navController2 = rememberNavController()
     val navBackStackEntry by navController2.currentBackStackEntryAsState()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-    val listState = rememberLazyListState()
     /*val currentRoute =
         navBackStackEntry?.destination?.route ?: WazzabyDrawerDestinations.HOME_ROUTE*/
     var switch by rememberSaveable { mutableStateOf(true) }
@@ -55,8 +73,17 @@ fun HomeApp(
             Route.homeTabView
         )
     )
+    userViewModel.getSavedToken()
+        .observe(LocalContext.current as LifecycleOwner) { tokenRoom -> }
+    val token by userViewModel.tokenValue.observeAsState()
+    if (!token?.token.isNullOrBlank()) {
+        userViewModel.getSavedUserByToken(token?.token!!)
+            .observe(LocalContext.current as LifecycleOwner) {}
+    }
+    val user by userViewModel.userValue.observeAsState()
+    var visibleSearch by remember { mutableStateOf(false) }
+
     Scaffold(topBar = {
-        //DrawerAppBar(scope, drawerState, "DiaspoPay",viewItem, context)
         Scaffold(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
@@ -187,7 +214,48 @@ fun HomeApp(
                         }
                     },
                     scrollBehavior = scrollBehavior,
-                    title = { Text("Yamba") }
+                    title = {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            AnimatedVisibility(
+                                visible = visibleSearch,
+                                enter = slideInHorizontally(animationSpec = tween(durationMillis = 200)) { fullWidth ->
+                                    // Offsets the content by 1/3 of its width to the left, and slide towards right
+                                    // Overwrites the default animation with tween for this slide animation.
+                                    -fullWidth / 3
+                                } + fadeIn(
+                                    // Overwrites the default animation with tween
+                                    animationSpec = tween(durationMillis = 200)
+                                ),
+                                exit = slideOutHorizontally(animationSpec = spring(stiffness = Spring.StiffnessHigh)) {
+                                    // Overwrites the ending position of the slide-out to 200 (pixels) to the right
+                                    200
+                                } + fadeOut()
+                            ) {
+                                if (user?.imageUrl?.length == 0) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.ic_profile_colorier),
+                                        modifier = Modifier.size(40.dp),
+                                        contentScale = ContentScale.Crop,
+                                        contentDescription = "Profile picture description"
+                                    )
+                                } else {
+                                    Image(
+                                        painter = rememberAsyncImagePainter("${BuildConfig.BASE_URL_DEV}/images/${user?.imageUrl}"),
+                                        modifier = Modifier
+                                            .height(40.dp)
+                                            .width(40.dp)
+                                            .clip(RoundedCornerShape(corner = CornerSize(20.dp))),
+                                        contentDescription = "Profile picture description"
+                                    )
+                                }
+                            }
+
+                            Text(
+                                modifier = Modifier.padding(start = 10.dp),
+                                text = "Yamba"
+                            )
+                        }
+                    }
                 )
             }) { innerPadding ->
 
@@ -237,4 +305,9 @@ fun HomeApp(
             }*/
         }, content = { innerPadding -> })
 
+    LaunchedEffect(true) {
+        delay(3)
+        visibleSearch = true
+    }
 }
+
