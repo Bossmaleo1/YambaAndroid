83fd3c5e4b36db1eef835cb3e6840ba867eaec74
sidneymaleo <sidney.maleo@socloz.com>
2023-04-02 19:56:14 +0200

    We add the location permission

---
+++ app/src/main/java/com/android/yambasama/ui/util/model/AddressLocation.kt
@@ -0,0 +1,9 @@
+package com.android.yambasama.ui.util.model
+
+data class AddressLocation (
+    val city: String?,
+    val country: String?,
+    val state: String?,
+    val zipCode: String?,
+    val countryCode: String?
+)
