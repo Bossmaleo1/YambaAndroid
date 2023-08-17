3adbe4c7d40d28fb830462983d89de66228e1e25
sidneymaleo <sidney.maleo@socloz.com>
2023-08-18 01:04:42 +0200

    we add our fakerepository and test our usercase

---
+++ app/src/androidTest/java/com/android/yambasama/ui/views/LoginViewKtTest.kt
@@ -0,0 +1,22 @@
+package com.android.yambasama.ui.views
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class LoginViewKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun outlined_textfield_email() {
+        composeTestRule.setContent {
+
+        }
+    }
+}
