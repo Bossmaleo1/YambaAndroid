dfb1f3d14e7438679cabceaf46616259528e5e9f
sidneymaleo <sidney.maleo@socloz.com>
2023-08-26 01:33:09 +0200

    we add get saved token use case

--- app/src/test/java/com/android/yambasama/domain/usecase/user/GetSavedTokenUseCaseTest.kt
+++ app/src/test/java/com/android/yambasama/domain/usecase/user/GetSavedTokenUseCaseTest.kt
@@ -1,7 +1,37 @@
 package com.android.yambasama.domain.usecase.user
 
+import app.cash.turbine.test
+import com.android.yambasama.domain.repository.FakeUserRepository
+import com.google.common.truth.Truth
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class GetSavedTokenUseCaseTest {
+
+    private lateinit var fakeUserRepository: FakeUserRepository
+    private lateinit var getSaveTokenUseCase: GetSavedTokenUseCase
+
+    @Before
+    fun setUp() {
+        fakeUserRepository = FakeUserRepository()
+        getSaveTokenUseCase = GetSavedTokenUseCase(fakeUserRepository)
+    }
+
+    @Test
+    fun getSavedToken_should_be_test() = runTest {
+        getSaveTokenUseCase.execute().test {
+            val tokenRoom = expectMostRecentItem()
+            Truth.assertThat(tokenRoom.id).isEqualTo(1)
+            Truth.assertThat(tokenRoom.token).isEqualTo("xxddjndfhfjf")
+        }
+    }
+
 
 }
