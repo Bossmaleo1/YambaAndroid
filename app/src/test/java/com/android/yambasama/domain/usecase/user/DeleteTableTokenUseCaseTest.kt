d807e9061c7f038d8e806e8cd3986e235172ee47
sidneymaleo <sidney.maleo@socloz.com>
2023-08-26 12:22:52 +0200

    we delete table token use case

--- app/src/test/java/com/android/yambasama/domain/usecase/user/DeleteTableTokenUseCaseTest.kt
+++ app/src/test/java/com/android/yambasama/domain/usecase/user/DeleteTableTokenUseCaseTest.kt
@@ -1,7 +1,26 @@
 package com.android.yambasama.domain.usecase.user
 
+import com.android.yambasama.domain.repository.FakeUserRepository
+import com.google.common.truth.Truth
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
 
 class DeleteTableTokenUseCaseTest {
 
+    private lateinit var fakeUserRepository: FakeUserRepository
+    private lateinit var deleteTableTokenUseCase: DeleteTableTokenUseCase
+
+    @Before
+    fun setUp() {
+        fakeUserRepository = FakeUserRepository()
+        deleteTableTokenUseCase = DeleteTableTokenUseCase(fakeUserRepository)
+    }
+
+    @Test
+    fun getDeleteTable_should_be_test() = runTest {
+        deleteTableTokenUseCase.execute()
+        Truth.assertThat(fakeUserRepository.tokenRooms.size).isEqualTo(0)
+    }
 }
