6da99ddc0dbd9c8dc88b0d28315d876bb3d986aa
sidneymaleo <sidney.maleo@socloz.com>
2023-08-26 14:31:00 +0200

    we initialize our service test

--- app/src/test/java/com/android/yambasama/data/api/service/AddressAPIServiceTest.kt
+++ app/src/test/java/com/android/yambasama/data/api/service/AddressAPIServiceTest.kt
@@ -1,4 +1,36 @@
 package com.android.yambasama.data.api.service
 
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class AddressAPIServiceTest {
+
+    private lateinit var service: AddressAPIService
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AddressAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(
+        fileName: String
+    ) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+    }
+
 }
