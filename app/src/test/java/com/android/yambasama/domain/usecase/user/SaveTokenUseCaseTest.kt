ac52f30a5d9ea06ac212cabe33d573d99182e8b6
sidneymaleo <sidney.maleo@socloz.com>
2023-08-26 11:42:40 +0200

    we upgrade save token usecase

--- app/src/test/java/com/android/yambasama/domain/usecase/user/SaveTokenUseCaseTest.kt
+++ app/src/test/java/com/android/yambasama/domain/usecase/user/SaveTokenUseCaseTest.kt
@@ -1,7 +1,38 @@
 package com.android.yambasama.domain.usecase.user
 
+import com.android.yambasama.data.model.dataLocal.TokenRoom
+import com.android.yambasama.domain.repository.FakeUserRepository
+import com.google.common.truth.Truth
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class SaveTokenUseCaseTest {
+    private lateinit var fakeUserRepository: FakeUserRepository
+    private lateinit var saveTokenUseCase: SaveTokenUseCase
 
+    @Before
+    fun setUp() {
+        fakeUserRepository = FakeUserRepository()
+        saveTokenUseCase = SaveTokenUseCase(fakeUserRepository)
+    }
+
+    @Test
+    fun SaveToken_should_be_test() = runTest {
+        fakeUserRepository.tokenRooms.removeAll(fakeUserRepository.tokenRooms)
+        saveTokenUseCase.execute(token = TokenRoom(id = 37, token = "tokenxxxffdsfds"))
+        Truth.assertThat(fakeUserRepository.tokenRooms.size).isEqualTo(1)
+        saveTokenUseCase.execute(token = TokenRoom(id = 40, token = "token2xxxffdsfds"))
+        saveTokenUseCase.execute(token = TokenRoom(id = 41, token = "token3xxxffdsfds"))
+        Truth.assertThat(fakeUserRepository.tokenRooms.size).isEqualTo(3)
+        Truth.assertThat(fakeUserRepository.tokenRooms[2].id).isEqualTo(41)
+        Truth.assertThat(fakeUserRepository.tokenRooms[1].token).isEqualTo("token2xxxffdsfds")
+
+    }
 }
