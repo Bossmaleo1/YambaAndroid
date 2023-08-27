716e3fd14bd18b8ec8dd8fb97395bd20cd6c147a
sidneymaleo <sidney.maleo@socloz.com>
2023-08-27 14:21:05 +0200

    we upgrade our payload

--- app/src/main/java/com/android/yambasama/data/model/api/ApiRefreshTokenResponse.kt
+++ app/src/main/java/com/android/yambasama/data/model/api/ApiRefreshTokenResponse.kt
@@ -2,9 +2,7 @@ package com.android.yambasama.data.model.api
 
 import com.google.gson.annotations.SerializedName
 
-data class ApiTokenResponse(
-    @SerializedName("token")
-    val token: String,
+data class ApiRefreshTokenResponse (
     @SerializedName("refresh_token")
     val refreshToken: String
 )
